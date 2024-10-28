35d86f73124fdcdc2279c5a177063fc5e322d3da
Oluwajuwon <oyaludejuwon@yahoo.com>
2024-10-28 23:48:30 +0100

    progress in top_k_elements

---
+++ NeetCode/arrays & hashing/top_k_elements (framework).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 23:39:38 2024
+
+@author: oyalu
+"""
+from typing import List
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+
+nums = [1,1,1,2,2,3]
+k = 2
+count = {}
+freq = [[] for i in range(len(nums) + 1)]
+print(freq)
+for n in nums:
+    count[n] = 1 + count.get(n,0)
+print(count)
+
+for n, c in count.items():
+    freq[c].append(n)
+print(freq)
+
+res = []
+for i in range(len(freq) -1, 0, -1):
+    print(i)
+    for n in freq[i]:
+        res.append(n)
+        if len(res) == k:
+            print(res)
--- NeetCode/arrays & hashing/top_k_elements.py
+++ NeetCode/arrays & hashing/top_k_elements.py
@@ -9,25 +9,44 @@ k = 2
 # nums = reversed(nums)
 h = {}
 
-for i in nums:
-    h[nums.count(i)] = i
+# for i in nums:
+#     h[nums.count(i)] = i
 
-print('hash',h)
-freq = list(h.keys())
-print('freq', freq)
+# print('hash',h)
+# freq = list(h.keys())
+# print('freq', freq)
 
-required = sorted(freq, reverse=True)[:k]
-print(required)
-print([h[req] for req in required])
+# required = sorted(freq, reverse=True)[:k]
+# print(required)
+# print([h[req] for req in required])
 # sorted_keys = sorted(h.keys(),reverse= True)
 # # print('descending keys', keys)
 # print([h[a] for a in keys])
 
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    h = {}
+# def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#     h = {}
 
-    for i in nums:
-        h[nums.count(i)] = i
+#     for i in nums:
+#         h[nums.count(i)] = i
 
-    freq = sorted(h.keys(), reverse= True)[:k]
-    return [h[req] for req in freq]
+#     freq = sorted(h.keys(), reverse= True)[:k]
+#     return [h[req] for req in freq]
+
+def get_key_by_value(dictionary, target_value):
+    return next(key for key, value in dictionary.items() if value == target_value)
+
+# Example dictionary
+fruits = {
+    "apple": "red",
+    "banana": "yellow",
+    "grape": "purple",
+    "lime": "green"
+}
+
+# Find the key for the value "yellow"
+fruit_name = get_key_by_value(fruits, "yellow")
+print(f"The fruit that is yellow is: {fruit_name}")
+
+# Find the key for the value "green"
+fruit_name = get_key_by_value(fruits, "green")
+print(f"The fruit that is green is: {fruit_name}")
